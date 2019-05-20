8c977b2723d6f580021397e73b2dd557cef04881
Russ Sayers <russell.sayers@gmail.com>
2019-05-20 09:15:13 -0700

    basic app

---
+++ src/main/java/clients/BasicProducer.java
@@ -0,0 +1,9 @@
+package clients;
+
+public class BasicProducer {
+    public static void main(String[] args) {
+        System.out.println("*** Starting Basic Producer ***");
+
+        // here will be the Kafka Producer code
+    }
+}
